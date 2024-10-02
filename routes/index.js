992aca6a9b302b6ea71f3802c15ca5b53a928cca
alejandra marino <alejamarino123@gmail.com>
2024-10-02 16:00:01 -0400

    add clothing router to index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRouter = require("./users");
-
 router.use("/users", userRouter);
+
+const clothingRouter = require("./clothingItems");
+router.use("/items", clothingRouter);
 
 module.exports = router;
