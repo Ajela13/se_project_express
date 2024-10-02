0fb0ee87f249cc077adc6d787b51fb7f4f3f0db2
alejandra marino <alejamarino123@gmail.com>
2024-10-02 15:05:19 -0400

    connect mongoose

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(console.log(console.error));
 
 app.listen(PORT, () => {
   console.log(`server is runing on port ${PORT}`);
