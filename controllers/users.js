681221bdd9455a317bed045db125d5c967283792
alejandra marino <alejamarino123@gmail.com>
2024-10-03 14:15:18 -0400

    add getUserbyId handler

--- controllers/users.js
+++ controllers/users.js
@@ -24,4 +24,19 @@ const createUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, createUser };
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+module.exports = { getUsers, createUser, getUserById };
