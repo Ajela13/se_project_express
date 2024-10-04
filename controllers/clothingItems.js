5fa7600ed84a6c3fe58d2dcbfdf7258e3bbd4679
alejandra marino <alejamarino123@gmail.com>
2024-10-04 13:13:08 -0400

    add delelte item, delete and update like handlers

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -30,4 +30,62 @@ const createItem = (req, res) => {
     });
 };
 
-module.exports = { getItems, createItem };
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  clothingItems
+    .findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const updateItemLike = (req, res) => {
+  const { itemId } = req.params;
+  clothingItems
+    .findByIdAndUpdate(itemId)
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const deleteItemLike = (req, res) => {
+  const { itemId } = req.params;
+  clothingItems
+    .findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  getItems,
+  createItem,
+  deleteItem,
+  updateItemLike,
+  deleteItemLike,
+};
+
+`PUT /items/:itemId/likes — like an item
+DELETE /items/:itemId/likes — unlike an item`;
