fe9d5cd162d63436ba5ab3238a313916801d5ab3
alejandra marino <alejamarino123@gmail.com>
2024-10-05 20:38:50 -0400

    update path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
-const { documentNotFoundError } = require("../utils/errors");
+const { documentNotFoundError } = require("./utils/errors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
