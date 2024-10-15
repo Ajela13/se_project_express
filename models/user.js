32decf3b4c5d651580601aa90eceba76a1895ba9
alejandra marino <alejamarino123@gmail.com>
2024-10-15 13:55:23 -0400

    create findUserByCredentials method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
@@ -26,5 +27,24 @@ const UserSchema = new mongoose.Schema({
   },
   password: { type: String, require: true },
 });
+
+UserSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect email or password"));
+    }
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return user; // now user is available
+    });
+  });
+};
 
 module.exports = mongoose.model("user", UserSchema);
