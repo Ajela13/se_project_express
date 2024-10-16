1219ab5fbfc9742b7c54f0ff3147275e3feb5917
alejandra marino <alejamarino123@gmail.com>
2024-10-16 13:35:09 -0400

    add /me get and patch routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
-const { login, createUser } = require("../controllers/users");
+const {
+  login,
+  createUser,
+  getCurrentUser,
+  modifyCurrentUser,
+} = require("../controllers/users");
 
 router.post("/signin", login);
 
 router.post("/signup", createUser);
+
+router.get("/me", getCurrentUser);
+
+router.patch("/me", modifyCurrentUser);
 module.exports = router;
