5d10b3e2d7c14001c07d38a234365ca09e327272
alejandra marino <alejamarino123@gmail.com>
2024-10-16 13:32:35 -0400

    update

--- controllers/users.js
+++ controllers/users.js
@@ -12,44 +12,24 @@ const { JWT_SECRET } = require("../utils/config");
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
 
-  return User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        const error = new Error(
-          "The user with the provided email already exists"
-        );
-        error.statusCode = CONFLICT_CODE;
-        throw error;
-      }
-
-      return bcrypt.hash(password, 10).then((hash) =>
-        User.create({
-          name,
-          avatar,
-          email,
-          password: hash,
-        })
-      );
-    })
-    .then((user) => {
-      const userWithoutPassword = user.toObject();
-      delete userWithoutPassword.password;
-      res.status(201).json(userWithoutPassword);
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST_CODE).send({ message: "Invalid data" });
-      }
-      if (err.statusCode === CONFLICT_CODE) {
+  bcrypt.hash(password, 10).then((hash) => {
+    User.create({ name, avatar, email, password: hash })
+      .then((user) => res.status(201).send(user))
+      .catch((err) => {
+        console.error(err);
+        if (err.statusCode === 11000) {
+          return res
+            .status(castError)
+            .send({ message: "Email already exists." });
+        }
+        if (err.name === "ValidationError") {
+          return res.status(castError).send({ message: "Invalid data" });
+        }
         return res
-          .status(CONFLICT_CODE)
-          .send({ message: "The user with the provided email already exists" });
-      }
-      return res
-        .status(INTERNAL_SERVICE_ERROR_CODE)
-        .send({ message: "Internal Service Error" });
-    });
+          .status(defaultError)
+          .send({ message: "An error has occurred on the server" });
+      });
+  });
 };
 
 const login = (req, res) => {
