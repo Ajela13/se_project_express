92561fbb70a503d67a04f282718ede5d4c9e856d
alejandra marino <alejamarino123@gmail.com>
2024-10-17 14:54:16 -0400

    modify name of route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
-const { getCurrentUser, modifyCurrentUser } = require("../controllers/users");
+const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
 
 router.use(auth);
 
 router.get("/me", getCurrentUser);
 
-router.patch("/me", modifyCurrentUser);
+router.patch("/me", updateCurrentUser);
 module.exports = router;
