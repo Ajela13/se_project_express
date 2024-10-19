abf00c3f231627b184d4f11b27b28452b0663547
alejandra marino <alejamarino123@gmail.com>
2024-10-19 10:44:53 -0400

    add const error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const { unauthorizedError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).send({ message: "Authorization required" });
+    return res
+      .status(unauthorizedError)
+      .send({ message: "Authorization required" });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -15,7 +18,9 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({ message: "Authorization Required" });
+    return res
+      .status(unauthorizedError)
+      .send({ message: "Authorization Required" });
   }
 
   req.user = payload;
