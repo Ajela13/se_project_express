38e2d6012de737f2c5a52babba4fd19015dbfd4d
alejandra marino <alejamarino123@gmail.com>
2025-01-13 22:06:24 -0500

    fix login error handling

--- controllers/users.js
+++ controllers/users.js
@@ -34,26 +34,36 @@ const createUser = (req, res, next) => {
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
+
+  // Check if both email and password are provided
   if (!email || !password) {
     return next(new CastError("Email and Password are REQUIRED!"));
   }
+
+  // Authenticate user
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      // Generate token upon successful login
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: "7d",
       });
-      return res.send({
+
+      // Send response with token and a 200 status code
+      return res.status(200).send({
         token,
       });
     })
     .catch((err) => {
       console.error(err);
+
       if (err.message === "Incorrect email or password") {
         return next(new UnauthorizedError("Incorrect email or password"));
       }
+
       return next(err);
     });
 };
+
 const getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
     .orFail(() => {
