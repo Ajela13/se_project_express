066f37b2d1f225d7735001f8afba1a61c8e2ad4c
alejandra marino <alejamarino123@gmail.com>
2025-01-14 14:09:51 -0500

    check statuscode

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 module.exports = (err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
+
+  if (!statusCode) {
+    console.error("Invalid status code:", err); // Log the invalid error if statusCode is missing
+  }
   res.status(statusCode).json({
     message: statusCode === 500 ? "Internal Server Error" : message,
   });
