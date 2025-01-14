85baf6d0358484009e93cb1367fee878e7210d8a
alejandra marino <alejamarino123@gmail.com>
2025-01-14 13:27:02 -0500

    add default key value

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,6 @@
-const { JWT_SECRET } = process.env;
+const {
+  JWT_SECRET = "0f3475fdcbdbf767c7abe86d73f401c3d696d40e05d1c64bfb57ec8cfa1deaf3",
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
