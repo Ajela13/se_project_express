6e1f37b15e980b0735a35df572094d99c4099d77
alejandra marino <alejamarino123@gmail.com>
2025-01-13 20:39:19 -0500

    add test review code

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+
+// remove after review
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.post("/signin", validateUserLoggedIn, login);
 app.post("/signup", validateUserCreated, createUser);
 app.use("/", mainRouter);
