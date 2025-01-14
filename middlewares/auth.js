6d0f37775300aafb78e43a0314a592532b3a5fc0
alejandra marino <alejamarino123@gmail.com>
2025-01-13 21:51:24 -0500

    add unauthorized error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,11 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { unauthorizedError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res
-      .status(unauthorizedError)
-      .send({ message: "Authorization required" });
+    return res.status(401).send({ message: "Authorization required" });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -19,11 +16,9 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     if (err.name === "TokenExpiredError") {
-      return res.status(unauthorizedError).json({ message: "Token expired" });
+      return res.status(401).json({ message: "Token expired" });
     }
-    return res
-      .status(unauthorizedError)
-      .send({ message: "Authorization Required" });
+    return res.status(401).send({ message: "Authorization Required" });
   }
 
   req.user = payload;
