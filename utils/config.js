07e36b8495a0b14b98c8bf865d1231e4802ea65f
alejandra marino <alejamarino123@gmail.com>
2025-01-15 15:04:16 -0500

    update const

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 const {
   JWT_SECRET = "0f3475fdcbdbf767c7abe86d73f401c3d696d40e05d1c64bfb57ec8cfa1deaf3",
+  NODE_ENV = "development",
 } = process.env;
 
 module.exports = {
   JWT_SECRET,
+  NODE_ENV,
 };
