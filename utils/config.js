1b565d9efe68e5cf295fa0782bd4f8fa7ac32242
Alejandra Mariño <53499295+Ajela13@users.noreply.github.com>
2025-04-09 18:03:24 -0500

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const {
-  JWT_SECRET = "0f3475fdcbdbf767c7abe86d73f401c3d696d40e05d1c64bfb57ec8cfa1deaf3",
-  NODE_ENV = "development",
+  JWT_SECRET ,
+  NODE_ENV,
 } = process.env;
 
 module.exports = {
